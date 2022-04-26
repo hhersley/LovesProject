22db9343699a6e928f0e6ab601aaf91a663452f1
hhersley <Hahersley@ou.edu>
2022-04-26 09:36:16 -0500

    style changes

--- TrialLovesProject/TrialLovesProject/Models/DatabaseLoves.Designer.cs
+++ TrialLovesProject/TrialLovesProject/Models/DatabaseLoves.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Student\Source\Repos\LovesProject\TrialLovesProject\TrialLovesProject\Models\DatabaseLoves.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Student\source\repos\LovesProject\TrialLovesProject\TrialLovesProject\Models\DatabaseLoves.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
