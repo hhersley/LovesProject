35e9ae69ac94597845970a43cbc9f7901352ae66
hhersley <Hahersley@ou.edu>
2022-05-02 00:20:13 -0500

    Progress! Finally!

--- TrialLovesProject/TrialLovesProject/Models/Grade.cs
+++ TrialLovesProject/TrialLovesProject/Models/Grade.cs
@@ -25,5 +25,6 @@ namespace TrialLovesProject.Models
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<StorePrice> StorePrices { get; set; }
+
     }
 }
